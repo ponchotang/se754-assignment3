197ed736be715d608a887c1516a351ec6e2ba86b
Andy Tang <andytang247@gmail.com>
2018-05-29 11:17:24 +1200

    Add test to verify keyword priority

--- src/test/java/se/a3/KeywordPriorityTest.java
+++ src/test/java/se/a3/KeywordPriorityTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import se.a3.*;
 
+import java.util.List;
+
 /**
  * Users can prioritise the keywords by changing the weights or order of the keywords.
  */
@@ -20,6 +22,7 @@ public class KeywordPriorityTest {
 
         //Then
         Assert.assertEquals("are Here five key words", swappedCollection.getString());
+        Assert.assertTrue(verifyKeywordPriority(swappedCollection));
     }
 
     @Test
@@ -56,5 +59,16 @@ public class KeywordPriorityTest {
         KeywordCollection collection = new KeywordCollection(userText);
         KeywordManager keywordManager = new KeywordManager(collection);
         KeywordCollection swappedCollection = keywordManager.changePriority(-1,0);
+    }
+
+    private boolean verifyKeywordPriority(KeywordCollection keywordCollection) {
+        List<Keyword> keywordList = keywordCollection.getList();
+
+        for (int i = 0; i < keywordList.size(); i++) {
+            if (keywordList.get(i).getWeight() != i + 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
